a4f85636eb67c974fc420d9163c6a2271b3669af
Luis Vega <luisevegam.91@gmail.com>
2021-06-11 17:24:29 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mayasoft-test',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
